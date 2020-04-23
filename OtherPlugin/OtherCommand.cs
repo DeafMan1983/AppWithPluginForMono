ce02aed6d2cc37b14f9c666168f87c96a349aac0
SourceSkyBoxer <57066679+SourceSkyBoxer@users.noreply.github.com>
2020-04-23 23:14:54 +0200

    Add files via upload

---
+++ OtherPlugin/OtherCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using PluginBase;
+namespace OtherPlugin
+{
+    public class OtherCommand : ICommand
+    {
+        public OtherCommand()
+        {
+        }
+
+        public string Name { get => "other"; }
+
+        public string Description { get => "Other message."; }
+
+        public int Execute()
+        {
+            Console.WriteLine("Other Plugin ;-) ");
+            return 0;
+        }
+    }
+}
