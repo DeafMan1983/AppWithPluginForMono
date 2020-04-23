683f7aec9a5e3feee4e7ddd80d83eec0fbb95018
SourceSkyBoxer <57066679+SourceSkyBoxer@users.noreply.github.com>
2020-04-23 23:13:42 +0200

    Add files via upload

---
+++ AppWithPlugin/Program.cs
@@ -0,0 +1,134 @@
+﻿using PluginBase;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+/*
+ *  AppWithPlugin for NetFX ( Don't use Dotnet 3.x or 5.x ) 
+ */
+namespace AppWithPlugin
+{
+    class Program
+    {
+        static Assembly asm;
+
+        /*
+         *  Search plugin *dll assemblies in Plugin directory
+         */
+        static string[] pluginPaths
+        {
+            get
+            {
+                string root = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Plugins"));
+                List<string> asmpaths = new List<string>();
+
+                foreach (string asmpath in Directory.EnumerateFiles(root, "*.dll"))
+                {
+                    if (File.Exists(asmpath))
+                    {
+                        asmpaths.Add(asmpath);
+                    }
+                }
+
+                return asmpaths.ToArray();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 1 && args[0] == "/d")
+                {
+                    Console.WriteLine("Waiting for any key...");
+                    Console.ReadLine();
+                }
+
+                IEnumerable<ICommand> commands = pluginPaths.SelectMany(pluginPath =>
+                {
+                    Assembly pluginAssembly = LoadPlugin(pluginPath);
+                    return CreateCommands(pluginAssembly);
+                }).ToList();
+
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("Commands: ");
+                    foreach (ICommand command in commands)
+                    {
+                        Console.WriteLine($"{command.Name}\t - {command.Description}");
+                    }
+                }
+                else
+                {
+                    foreach (string commandName in args)
+                    {
+                        Console.WriteLine($"-- {commandName} --");
+                        ICommand command = commands.FirstOrDefault(c => c.Name == commandName);
+                        if (command == null)
+                        {
+                            Console.WriteLine("No such command is known.");
+                            return;
+                        }
+
+                        /*
+                         *  For old netfx version if you are not using Dotnet :-D
+                         *  It works fine without Dotnet :-P                        
+                         */
+                        foreach (Type type in asm.GetExportedTypes())
+                        {
+                            var o = Activator.CreateInstance(type);
+                            o = command.Execute();
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static Assembly LoadPlugin(string relativePath)
+        {
+            // Navigate up to the solution root
+            string root = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Plugins"));
+
+            string pluginLocation = Path.GetFullPath(Path.Combine(root, relativePath.Replace('/', Path.DirectorySeparatorChar)));
+            //Console.WriteLine($"Loading commands from: {pluginLocation}");
+            /*
+             *  Eh it works fine for old version :-D
+             */
+            asm = Assembly.LoadFile(pluginLocation);
+            return asm = Assembly.Load(new AssemblyName(Path.GetFileNameWithoutExtension(pluginLocation)));
+        }
+
+        static IEnumerable<ICommand> CreateCommands(Assembly assembly)
+        {
+            int count = 0;
+
+            foreach (Type type in assembly.GetTypes())
+            {
+                if (typeof(ICommand).IsAssignableFrom(type))
+                {
+                    ICommand result = Activator.CreateInstance(type) as ICommand;
+                    if (result != null)
+                    {
+                        count++;
+                        yield return result;
+                    }
+                }
+            }
+
+            if (count == 0)
+            {
+                string availableTypes = string.Join(",", assembly.GetTypes().Select(t => t.FullName));
+                throw new ApplicationException(
+                    $"Can't find any type which implements ICommand in {assembly} from {assembly.Location}.\n" +
+                    $"Available types: {availableTypes}");
+            }
+        }
+    }
+}
